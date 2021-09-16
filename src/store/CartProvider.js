07c9b2f6dc781cbfb01ab4ae73e2dbef19a7da85
Michael Towson <MTowson@gmail.com>
2021-09-16 16:42:48 -0700

    add submit logic for adding items to cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,17 +1,45 @@
-
+import {useReducer} from 'react';
 
 import CartContext from './cart-context';
 
 //NOTE: Because app.js is going to be using this context, all of this logic could have also been put in there. However, by putting this into a wrapper component, we keep the file lean.
 
-const CartProvider = props => {
-  const addItemToCartHandler = item => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
 
-  const removeItemFromCartHandler = item => {};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    
+    //concat returns a new array. We don't want to edit previous state reference
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+
+  }
+  return defaultCartState
+};
+
+
+const CartProvider = props => {
+  //Initialize reducer
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = item => {
+    dispatchCartAction({type: 'ADD', item: item});
+  };
+
+  const removeItemFromCartHandler = item => {
+    dispatchCartAction({type: 'REMOVE', id: item.id});
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
