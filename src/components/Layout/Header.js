33630a6eb0d5fe163c5d48fcf485ba501b1a0b7f
Michael Towson <MTowson@gmail.com>
2021-09-16 15:27:44 -0700

    add ability to open and close cart modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header =  props => {
   return <Fragment>
     <header className={classes.header}>
       <h1>REACT Meals</h1>
-      <HeaderCartButton/>
+      <HeaderCartButton onShowCart={props.onShowCart}/>
     </header>
     <div className={classes['main-image']}>
       <img src={imgMeals} alt="A table full of delicious food"/>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from'react-dom'
 import classes from './Modal.module.css'
 
 const Backdrop = props => {
-  return <div className={classes.backdrop} />
+  return <div className={classes.backdrop} onClick={props.onClose} />
 };
 
 const ModalOverlay = props => {
@@ -15,11 +15,11 @@ const ModalOverlay = props => {
 }
 
 // Create target to portal, which was defined in index.html
-const portalElement = document.getElemenitById('overlays'); 
+const portalElement = document.getElementById('overlays'); 
 
 const Modal = props => {
   return <Fragment>
-    {ReactDOM.createPortal(<Backdrop />, portalElement)}
+    {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
     {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
   </Fragment>
 };
